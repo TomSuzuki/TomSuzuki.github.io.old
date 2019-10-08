57e0960322f8221bf081e36b7fbd116688fef204
TomSuzuki-No.14 <tomsuzuki2633@gmail.com>
2019-10-08 09:12:22 +0900

    シンプルに

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function showContents() {
+    document.getElementById("top").style.animationPlayState = "running";
+    document.getElementById("contents").style.animationPlayState = "running";
+    document.getElementById("contents").style.display = "block";
+}
