27a57d409cae7abd59f78d3d466e6c8653c1cfee
TomSuzuki-No.14 <tomsuzuki2633@gmail.com>
2019-10-08 12:45:21 +0900

    window原型

--- js/index.js
+++ js/index.js
@@ -3,3 +3,9 @@ function showContents() {
     document.getElementById("contents").style.animationPlayState = "running";
     document.getElementById("contents").style.display = "block";
 }
+
+// デバッグ用
+window.addEventListener("DOMContentLoaded", function () {
+    showContents();
+    modalOpen("./html/portfolio.html","ポートフォリオ");
+});
---
+++ js/modal.js
@@ -0,0 +1,27 @@
+function modalClose() {
+    document.getElementById("modal").style.display = "none";
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('modalWindow').addEventListener('click', (e) => e.stopPropagation());
+});
+
+function modalOpen(path, title) {
+    contentWindow("Now Loading...", "Now Loading...");
+    document.getElementById("modal").style.display = "block";
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', `${path}`, true);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function () {
+        loadTextFile(([`./html/error.html`, `${path}`])[Number(xhr.status === 200)], function (result) {
+            contentWindow(([`Error - 404 - File not found`, `./html/${title}.html`])[Number(xhr.status === 200)], result);
+        });
+    }
+    xhr.send();
+}
+
+function contentWindow(title, text) {
+    document.getElementById("modalBarTitle").innerHTML = title;
+    document.getElementById("modalContent").innerHTML = text;
+}
