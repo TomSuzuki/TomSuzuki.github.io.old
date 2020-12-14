9e4b0dd7e4a6b80ff0e2d291cd71d4f682c194a5
TomSuzuki-No.13 <tomsuzuki2633@gmail.com>
2020-12-15 01:58:03 +0900

    [fix] list

--- make.bat
+++ make.bat
@@ -8,8 +8,10 @@ set count=0
 for /F %%i in ('dir /B .\\md\\*.md ^| find /c /v ""') do @set lines=%%i
 
 echo [ > %filename%
-for /f "usebackq" %%i in (`dir /B .\\md\\*.md`) do (
+for /f "usebackq delims=" %%i in (`dir /B .\\md\\*.md`) do (
     set /a count=count+1
+
+    echo %%i
 
     if !count!==%lines% (
         echo 	"./md/%%i" >> %filename%
