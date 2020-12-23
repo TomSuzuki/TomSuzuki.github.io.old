6f049eba4766bd6061a953a4c9246f9646530925
TomSuzuki-No.13 <tomsuzuki2633@gmail.com>
2020-12-23 15:22:27 +0900

    [fix] replaceAll

--- js/function.js
+++ js/function.js
@@ -1,33 +1,15 @@
 // replaceAll
 function replaceAll(string, target, replacement) {
-    var result = "";
-    var offset = 0;
-    var target_length = target.length;
-    if (target_length === 0) {
-        for (var i = 0, c = string.length; i < c; i++) {
-            result += string[i];
-            result += replacement;
-        }
-        if (result.length)
-            return result.substr(0, result.length - replacement.length);
-        return result;
-    }
     do {
-        var i = string.indexOf(target, offset);
-        if (i === -1) {
-            result += string.substring(offset);
-            return result;
-        }
-        result += string.substring(offset, i);
-        result += replacement;
-        offset = i + target_length;
+        if (string.indexOf(target) == -1) return string;
+        string = string.replace(target, replacement);
     } while (true);
 }
 
 // jsonToArray
-function jsonToArray(list){
+function jsonToArray(list) {
     let s = [];
-    for(let i in list){
+    for (let i in list) {
         s.push(list[i]);
     }
     return s;
