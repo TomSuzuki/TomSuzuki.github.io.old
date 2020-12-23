939dc16c8f0879dee98b4badc70515ff89d08273
TomSuzuki-No.13 <tomsuzuki2633@gmail.com>
2020-12-23 13:33:13 +0900

    [fix] createXMLHttpRequest

--- js/loadFile.js
+++ js/loadFile.js
@@ -16,15 +16,7 @@ function createXMLHttpRequest() {
 	try {
 		XMLhttpObject = new XMLHttpRequest();
 	} catch (e) {
-		try {
-			XMLhttpObject = new ActiveXObject("Msxml2.XMLHTTP");
-		} catch (e) {
-			try {
-				XMLhttpObject = new ActiveXObject("Microsoft.XMLHTTP");
-			} catch (e) {
-				return null;
-			}
-		}
+		return null;
 	}
 	return XMLhttpObject;
 }
